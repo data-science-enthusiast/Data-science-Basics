c9cf40bd64ec58fa854c5d93c3f188833f646e87
||Ash|| <31936539+data-science-enthusiast@users.noreply.github.com>
2018-02-20 15:35:17 -0500

    Update Multivariate.py

--- Multivariate/Multivariate.py
+++ Multivariate/Multivariate.py
@@ -1,7 +1,6 @@
 '''
-Generate   100   3-dimensional   vectors   that   come   from   a   normal   distribution   with  
-mean vector   as   [1   2   1]t   and   3x3  
-covariance   matrix   as   [4   0.8   -0.3;   0.8   2   0.6;   -0.3   0.6   5]
+Generate   100   3-dimensional   vectors   that   come   from   a   normal   distribution   with given
+mean vector   and covariance   matrix .
 Make   scatter   plots   of   x1   vs   x2,   x1   vs   x3,   and   x2   vs   x3  to explore  relationships'''
 
 import    matplotlib.pyplot    as    plt
